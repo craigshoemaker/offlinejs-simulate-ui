7ae6a898a27ab87334f5550077572674d848b60f
Craig Shoemaker <craig@craigshoemaker.net>
2014-03-18 10:44:29 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-offlinejs-simulate-ui
-=====================
+# Offline.js Simulate UI Plug-In
 
 Offline.js plug-in that allows you to test how your pages respond to different connectivity states without having to use brute-force methods to disable your actual connectivity.
---
+++ js/simulate-ui.js
@@ -0,0 +1,54 @@
+(function() {
+  (function(Offline, document) {
+    var forceFail, i, scripts;
+    if (!Offline) {
+      throw new Error("Offline simulate UI brought in without Offline.js");
+    }
+    console.info("The offline.simulate.ui.js module is a development-only resource. Make sure to remove offline.simulate.ui.js in production.");
+    Offline.options.reconnect = false;
+    scripts = document.getElementsByTagName("script");
+    forceFail = "data-force-fail";
+    i = 0;
+    while (i < scripts.length) {
+      if (scripts[i].getAttribute(forceFail) && scripts[i].getAttribute(forceFail) === "true") {
+        Offline.options.isSimulating = true;
+      }
+      i++;
+    }
+    Offline.forceChecksToFail = function() {
+      Offline.options.checks = {
+        image: {
+          url: "simulate-offline-state.png"
+        },
+        active: "image"
+      };
+    };
+    if (Offline.options.isSimulating) {
+      Offline.forceChecksToFail();
+    }
+    document.addEventListener("DOMContentLoaded", (function() {
+      var STYLE, TEMPLATE, body, container, styleElement;
+      STYLE = "<style>" + ".offline-simulate-ui {" + "    position: fixed;" + "    z-index: 100000;" + "    left: -4px;" + "    top: 45%;" + "    border: solid 1px rgba(0, 0, 0, 0.15);" + "    -webkit-border-radius: 4px 4px 4px 4px;" + "    -moz-border-radius: 4px 4px 4px 4px;" + "    -ms-border-radius: 4px 4px 4px 4px;" + "    -o-border-radius: 4px 4px 4px 4px;" + "    border-radius: 4px 4px 4px 4px;" + "    font-family: \"Lucida Grande\", sans-serif;" + "    font-size: 12px;" + "    padding: 2px;" + "    padding-left: 6px;" + "    width: 25px;" + "    background: #f6f6f6;" + "    color: #888888;" + "}" + "</style>";
+      styleElement = document.createElement("div");
+      styleElement.innerHTML = STYLE;
+      body = document.getElementsByTagName("body")[0];
+      body.appendChild(styleElement);
+      TEMPLATE = "<input type=\"checkbox\" id=\"offline-simulate-check\" title=\"Simulate online/offline states\">";
+      container = document.createElement("div");
+      container.className = "offline-simulate-ui";
+      container.innerHTML = TEMPLATE;
+      body.appendChild(container);
+      document.getElementById("offline-simulate-check").addEventListener("click", (function(e) {
+        if (this.checked) {
+          Offline.markDown();
+        } else {
+          Offline.markUp();
+        }
+      }), false);
+      Offline.on("confirmed-up", function() {
+        document.getElementById("offline-simulate-check").checked = false;
+      });
+    }), false);
+  })(window.Offline, window.document);
+
+}).call(this);
